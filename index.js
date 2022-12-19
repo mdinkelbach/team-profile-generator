826847428e4ffe69027349eccbf6814650e5c44a
mdinkelbach <mj.dinkelbach@mgail.com>
2022-12-18 18:29:01 -0600

    inquirer2

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const inquirer = require("inquirer");
 const Manager = require("./lib/manager").Manager;
-const Engineer = require("./lib/engineer").Engineer;
-const Intern = require("./lib/intern").Intern;
 const Employee = require("./lib/employee").Employee;
 
 
